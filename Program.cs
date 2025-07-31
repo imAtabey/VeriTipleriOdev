16272ca0c59b8463dacccf6dd579ff9fa084e14a
Hüseyin ÜTEBAY <huseyin.utebay@gmail.com>
2025-07-31 12:32:33 +0300

    Operatorler Ders Odevi Eklendi

--- Program.cs
+++ Program.cs
@@ -3,72 +3,139 @@ internal class NewBaseType
 {
     private static void Main(string[] args)
     {
-        // int deger; deger=2; -> int türünde oluşturup, değeri atandı
-        //string degisken veri; -> boşluk olamaz
-        //string adı+soyadı; -> olmaz sadece "_" kullanılabilir
-        //string 1deneme; -> olmaz - rakam ile başlayamaz
-        // string namespace; -> olmaz, özel isimler kullanılamaz
+        #region Degiskenler
+        /*
+                // int deger; deger=2; -> int türünde oluşturup, değeri atandı
+                //string degisken veri; -> boşluk olamaz
+                //string adı+soyadı; -> olmaz sadece "_" kullanılabilir
+                //string 1deneme; -> olmaz - rakam ile başlayamaz
+                // string namespace; -> olmaz, özel isimler kullanılamaz
 
-        byte b = 5;         // -> 1 byte
-        sbyte sb = 5;        // -> 1 byte
+                byte b = 5;         // -> 1 byte
+                sbyte sb = 5;        // -> 1 byte
 
-        short s = 5;        // -> 2 byte
-        ushort us = 5;      // -> 2 byte - short aralığında ama pozitif değerler
-        Int16 i16 = 15;     // -> 2 byte 
-        int i = 2;          //-> 4 byte
-        Int32 i32 = 4;      //-> 4 byte
-        Int64 i64 = 8;      //-> 8 byte
-        uint ui = 5;        //-> 4 byte
-        long l = 4;         //-> 8 byte
-        ulong ul = 4;       //-> 8 byte
-        float f = 5;        //-> 4 byte
-        double d = 5;       //-> 8 byte
-        decimal de = 5;     //->16 byte
+                short s = 5;        // -> 2 byte
+                ushort us = 5;      // -> 2 byte - short aralığında ama pozitif değerler
+                Int16 i16 = 15;     // -> 2 byte 
+                int i = 2;          //-> 4 byte
+                Int32 i32 = 4;      //-> 4 byte
+                Int64 i64 = 8;      //-> 8 byte
+                uint ui = 5;        //-> 4 byte
+                long l = 4;         //-> 8 byte
+                ulong ul = 4;       //-> 8 byte
+                float f = 5;        //-> 4 byte
+                double d = 5;       //-> 8 byte
+                decimal de = 5;     //->16 byte
 
-        char c = 'c';       //-> 3 byte
-        string st = "";     //-> sınırsız
+                char c = 'c';       //-> 3 byte
+                string st = "";     //-> sınırsız
 
-        bool bo = false;    //-> true,false olabilir.
-        DateTime dt = DateTime.Now; //-> çalışma zamanının anlık tarih saat bilgisini getirir.
+                bool bo = false;    //-> true,false olabilir.
+                DateTime dt = DateTime.Now; //-> çalışma zamanının anlık tarih saat bilgisini getirir.
 
-        object o = 5.43; //-> her tür veriyi tutabilir.
+                object o = 5.43; //-> her tür veriyi tutabilir.
 
-        //string ifadeler
-        string str = "";
-        string str1 = null;
-        string str2 = string.Empty;
-        string ad = "Hüseyin";
-        string soyad = "ÜTEBAY";
-        string tamIsim = ad + " " + soyad;
+                //string ifadeler
+                string str = "";
+                string str1 = null;
+                string str2 = string.Empty;
+                string ad = "Hüseyin";
+                string soyad = "ÜTEBAY";
+                string tamIsim = ad + " " + soyad;
 
-        //integer tanımlama şekilleri
-        int i1 = 5;
-        int i2 = 3;
-        int i3 = i1 * i2;
+                //integer tanımlama şekilleri
+                int i1 = 5;
+                int i2 = 3;
+                int i3 = i1 * i2;
 
-        //boolean kullanım örnekleri
-        bool b1 = 10 < 2; //-> false döner b1=false; ile aynı
+                //boolean kullanım örnekleri
+                bool b1 = 10 < 2; //-> false döner b1=false; ile aynı
 
-        //Değişken dönüşümleri
-        string str20 = "20";
-        int i20 = 20;
-        string yeniDeger = str20 + i20.ToString(); // i20 string'e dönüştürüldü.
+                //Değişken dönüşümleri
+                string str20 = "20";
+                int i20 = 20;
+                string yeniDeger = str20 + i20.ToString(); // i20 string'e dönüştürüldü.
 
-        string str15 = "15";
-        int i30 = 30;
-        int yeniSayi = Convert.ToInt32(str15) + i30;
+                string str15 = "15";
+                int i30 = 30;
+                int yeniSayi = Convert.ToInt32(str15) + i30;
 
-        //datetime
-        string date = DateTime.Now.ToString("dd.MM.yyyy");
-        Console.WriteLine(date);
+                //datetime
+                string date = DateTime.Now.ToString("dd.MM.yyyy");
+                Console.WriteLine(date);
 
-        string date1 = DateTime.Now.ToString("dd/MM/yyyy");
-        Console.WriteLine(date1);
+                string date1 = DateTime.Now.ToString("dd/MM/yyyy");
+                Console.WriteLine(date1);
 
-        string hour = DateTime.Now.ToString("HH:mm");
-        Console.WriteLine(hour);
+                string hour = DateTime.Now.ToString("HH:mm");
+                Console.WriteLine(hour);
 
-        //Github içine yüklendi
+                //Github içine yüklendi
+                */
+        #endregion
+        #region Operatorler
+        
+         /*
+   int x = 3;
+       int y = 3;
+       y = y + 2;
+       Console.WriteLine(y);
+       y += 2;
+       Console.WriteLine(y);
+       y /= 3;
+       Console.WriteLine(y);
+       x *= 2;
+       Console.WriteLine(x);
+
+       //Mantıksal Operatörler &&,||,!
+       bool isSuccess = true;
+       bool isCompleted = false;
+
+       if (isSuccess && isCompleted) //ve
+       {
+           Console.WriteLine("Perfect");
+       }
+       if (isSuccess || isCompleted) //veya
+       {
+           Console.WriteLine("Great");
+       }
+       if (isSuccess && !isCompleted) //isCompleted true döner
+       {
+           Console.WriteLine("Fine");
+       }
+
+       //İlişkisel operatörler <,>,<=,=>,==,!=
+       int a = 3;
+       int b = 4;
+       bool sonuc = a < b;
+       Console.WriteLine(sonuc);
+       sonuc = a > b;
+       Console.WriteLine(sonuc);
+       sonuc = a >= b;
+       Console.WriteLine(sonuc);
+       sonuc = a <= b;
+       Console.WriteLine(sonuc);
+       sonuc = a == b;
+       Console.WriteLine(sonuc);
+       sonuc = a != b;
+       Console.WriteLine(sonuc);
+
+       //Matematiksel ifadeler
+       int sayi1 = 10;
+       int sayi2 = 5;
+       int sonuc1 = sayi1 / sayi2;
+       Console.WriteLine(sonuc);
+       sonuc1 = sayi1 * sayi2;
+       Console.WriteLine(sonuc);
+       sonuc1 = sayi1 + sayi2;
+       Console.WriteLine(sonuc);
+       sonuc1 = sayi1++;
+
+       // % Mod Alır
+       int sonuc2 = 20 % 3;
+       Console.WriteLine(sonuc2);
+      */
+        #endregion
 
     }
 }
